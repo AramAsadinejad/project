720a3911d26eb16220b56e2fae55581dca613fa3
AramAsadinejad <jeonaram57@gmail.com>
2025-07-18 18:13:40 +0330

    changes

--- project3/bond_list.py
+++ project3/bond_list.py
@@ -1,20 +1,27 @@
 import pandas as pd
 from datetime import datetime, timedelta
-
+import json
 def get_bonds_list(date):
-    df = pd.read_csv("transactions.csv")
-    bonds_list = []
-    df['Date'] = pd.to_datetime(df['Date'])
+    # df = pd.read_csv("transactions.csv")
+    # bonds_list = []
+    # df['Date'] = pd.to_datetime(df['Date'])
     
-    # شروع از تاریخ ورودی
-    date = pd.to_datetime(date)
+    # # شروع از تاریخ ورودی
+    # date = pd.to_datetime(date)
 
-    for i in range(3):
-        # فیلتر بر اساس تاریخ و نوع
-        filtered = df[(df['Date'] == date) & (df['type'].str.lower() == 'bonds')]
+    # for i in range(3):
+    #     # فیلتر بر اساس تاریخ و نوع
+    #     filtered = df[(df['Date'] == date) & (df['type'].str.lower() == 'bonds')]
         
-        if not filtered.empty:
-            return float(filtered.iloc[0]['price'])
+    #     if not filtered.empty:
+    #         return float(filtered.iloc[0]['price'])
 
-        # رفتن به ۷ روز قبل‌تر
-        date -= timedelta(days=7)
+    #     # رفتن به ۷ روز قبل‌تر
+    #     date -= timedelta(days=7)
+    with open("data.json", 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    bonds_list = [0,0,0,0]
+    bonds_list[1] = data.get("last_week_bond")
+    bonds_list[2] = data.get("last_two_week_bond")
+    bonds_list[3] = data.get("last_three_week_bond")
